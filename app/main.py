85d95d2b5e02811bdf662ad8e3254ef6ee9083ad
sina hosseinizad <ssh@ammonit.com>
2024-09-18 17:13:56 +0200

    stage 2 WIP

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket  # noqa: F401
-
+def BrokerResponse(socket, response):
+    socket.sendall(response)
 
 def main():
     # You can use print statements as follows for debugging,
@@ -11,9 +12,10 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     ClientSock, ClientAddr = server.accept() # wait for client
     data = ClientSock.recv(1024)
-    print(f'data received from {ClientAddr} is {data}')
+    print(f'data received from {ClientAddr} is {data} and its binary lenght is {data.__len__()} and the 7 and 8 members are {data[6:8]}')
     response = b"\x00\x00\x00\x00\x00\x00\x00\x07"
     ClientSock.sendall(response)
+    BrokerResponse(ClientSock, response)
     
 
 if __name__ == "__main__":
